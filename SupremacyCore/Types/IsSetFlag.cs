065c2865ffe23263a266619a78b95d30c976660a
David William Richmond Davies-Jones <david@exultantmonkey.co.uk>
2018-07-29 13:51:50 +0100

    Convert IsSetFlag to C#

---
+++ SupremacyCore/Types/IsSetFlag.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supremacy.Types
+{
+    [Serializable]
+    public sealed class IsSetFlag
+    {
+        private bool _isSet;
+
+        public IsSetFlag(bool isSet)
+        {
+            _isSet = isSet;
+        }
+
+        public bool IsSet
+        {
+            get { return _isSet; }
+        }
+
+        public void Set()
+        {
+            _isSet = true;
+        }
+    }
+}
