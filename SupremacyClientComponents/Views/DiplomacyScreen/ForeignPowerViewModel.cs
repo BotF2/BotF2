f10dca6b7d649744240637df83002646724d65b3
Reginald <bote-reginald@gmx.de>
2018-09-01 17:34:31 +0200

    issue 13  - brought in Description into DiploScreen and re-arranged Meter for Regard and Trust

--- SupremacyClientComponents/Views/DiplomacyScreen/ForeignPowerViewModel.cs
+++ SupremacyClientComponents/Views/DiplomacyScreen/ForeignPowerViewModel.cs
@@ -57,6 +57,11 @@ namespace Supremacy.Client.Views
             get { return _foreignPower.Counterparty; }
         }
 
+        public String CounterpartyDiplomacyReport
+        {
+            get { return _foreignPower.Counterparty.DiplomacyReport; }
+        }
+
         public Civilization Owner
         {
             get { return _foreignPower.Owner; }
