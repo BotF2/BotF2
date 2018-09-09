c19cffe9268a9dff1dd78dd69ba913664fa1e403
Kenneth Flanagan <kenneth.of.borg@googlemail.com>
2018-09-09 07:43:45 -0700

    Move to Game folder of Core

---
+++ SupremacyCore/Game/ResearchBreakthroughs.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2009 Mike Strobel
+//
+// This source code is subject to the terms of the Microsoft Reciprocal License (Ms-RL).
+// For details, see <http://www.opensource.org/licenses/ms-rl.html>.
+//
+// All other rights reserved.
+
+using Supremacy.Game;
+using Supremacy.Universe;
+using Supremacy.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Supremacy.Game
+
+{
+    [Serializable]
+    public class  ReserchBreakthroughs : GameEngine
+    {      
+        bool m_traceNebulaProtomater = true;
+
+        if (m_traceNebulaProtomater)
+        {
+            //GameLog.Print("Colony = {0}, population after = {1}, health after = {2}", targetColonyId, GameContext.Current.Universe.Get<Colony>(targetColonyId).Population.CurrentValue, GameContext.Current.Universe.Get<Colony>(targetColonyId).Health.CurrentValue);
+        }
+
+        GameContext.Current.Universe.UpdateSectors();
+
+                    return;
+                
+            
+        
+    
+    }
