d01594be081aac5016ceb1685a3ba8c2cb6d51d5
Reginald <bote-reginald@gmx.de>
2019-02-23 11:39:34 +0100

    NewShipSelectionViw should now work

--- SupremacyClientComponents/Views/ColonyScreen/NewShipSelectionView.xaml.cs
+++ SupremacyClientComponents/Views/ColonyScreen/NewShipSelectionView.xaml.cs
@@ -1,36 +1,33 @@
-﻿using Microsoft.Practices.Composite.Presentation.Commands;
-using Supremacy.Economy;
+﻿using Supremacy.Economy;
 using Supremacy.Game;
 using Supremacy.Tech;
-using Supremacy.Universe;
-using System;
+using Supremacy.Utility;
+using Supremacy.Client;
 using System.Linq;
+
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Input;
 
-using CompositeRegionManager = Microsoft.Practices.Composite.Presentation.Regions.RegionManager;
-
 namespace Supremacy.Client.Views
 {
+
+
     public partial class NewShipSelectionView
     {
-        private string _builderKey;
-      
-
         public NewShipSelectionView(ShipyardBuildSlot buildSlot)
         {
             InitializeComponent();
+
+            
 
             BuildProject[] shipList = TechTreeHelper.GetShipyardBuildProjects(buildSlot.Shipyard)
                                         .OrderByDescending(s => s.BuildDesign.BuildCost)
                                         .ToArray();
 
             BuildProjectList.ItemsSource = shipList;
-
-            _builderKey = shipList[0].Builder.Key; // set up for the switch in ShipInfoByEmpire
 
             SetBinding(
                 SelectedBuildProjectProperty,
@@ -59,59 +56,6 @@ namespace Supremacy.Client.Views
         }
         #endregion
 
-        #region ShipInfoByEmpire Property
-        public string ShipInfoByEmpire
-        {
-            get
-            {
-                string imagePath = "vfs:///Resources/UI/Default/Ship_Functions.png";
-                switch (_builderKey)
-                {
-                    case "FEDERATION":
-                        imagePath = "vfs:///Resources/UI/Federation/ColonyScreen/Ship_Functions.png";
-                        break;
-                    case "KLINGONS":
-                        imagePath = "vfs:///Resources/UI/Klingons/ColonyScreen/Ship_Functions.png";
-                        break;
-                    case "ROMULANS":
-                        imagePath = "vfs:///Resources/UI/Borg/ColonyScreen/Ship_Functions.png";
-                        break;
-                    case "DOMINION":
-                        imagePath = "vfs:///Resources/UI/Dominion/ColonyScreen/Ship_Functions.png";
-                        break;
-                    case "TERRANEMPIRE":
-                        imagePath = "vfs:///Resources/UI/TerranEmpire/ColonyScreen/Ship_Functions.png";
-                        break;
-                    case "BORG":
-                        imagePath = "vfs:///Resources/UI/Borg/ColonyScreen/Ship_Functions.png";
-                        break;
-                    case "CARDASSIANS":
-                        imagePath = "vfs:///Resources/UI/Cardassians/ColonyScreen/Ship_Functions.png";
-                        break;
-                    default:
-                        imagePath = "vfs:///Resources/UI/Default/Ship_Functions.png";
-                        break;
-                }
-                return imagePath;
-            }
-            set
-            {
-                if (SelectedBuildProject == null)
-                {
-                    var property = DependencyProperty.Register(
-                         "SelectedBuildProject",
-                         typeof(ShipBuildProject),
-                         typeof(NewShipSelectionView),
-                         new PropertyMetadata());
-                    var project = (ShipBuildProject)GetValue(property);
-                    _builderKey = project.Builder.Key;
-                    _builderKey = value;
-                }
-                SelectedBuildProject.Builder.Key = value;
-            }
-        }
-        #endregion
-        
         #region AdditionalContent Property
         public static readonly DependencyProperty AdditionalContentProperty = DependencyProperty.Register(
             "AdditionalContent",
@@ -128,6 +72,18 @@ namespace Supremacy.Client.Views
         }
         #endregion
 
+        
+
+        public string ShipFunctionPath 
+        {
+            get
+            {
+                return "vfs:///Resources/UI/" + Context.DesignTimeAppContext.Instance.LocalPlayerEmpire.Civilization + "/ColonyScreen/Ship_Functions.png";
+            }
+        }
+
+
+
         private void CanExecuteAcceptCommand(object sender, CanExecuteRoutedEventArgs e)
         {
             e.CanExecute = (SelectedBuildProject != null);
@@ -139,6 +95,8 @@ namespace Supremacy.Client.Views
                 return;
             DialogResult = true;
         }
+
+
 
         private void OnBuildProjectListMouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
