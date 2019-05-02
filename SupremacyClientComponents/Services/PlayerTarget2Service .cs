c17579ccc5b1b4c1a4d43290ec653e53da088635
Kenneth Flanagan <kenneth.of.borg@gmail.com>
2019-05-02 14:25:27 -0700

    Updated code for transmitting primary and secondary target selection to combat code. Need to test the results but the game runs.

---
+++ SupremacyClientComponents/Services/PlayerTarget2Service .cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using Supremacy.Annotations;
+using Supremacy.Game;
+using Supremacy.Client.Context;
+
+namespace Supremacy.Client.Services
+{
+    public sealed class PlayerTarget2Service : IPlayerTarget2Service
+    {
+        public static PlayerTarget2Service Instance = null;
+        private readonly IAppContext _appContext;
+        private readonly List<Order> _target2;
+
+        public bool AutoTurnTarget2 { get; set; }
+
+        public PlayerTarget2Service([NotNull] IAppContext appContext)
+        {
+            if (appContext == null)
+                throw new ArgumentNullException("appContext");
+            _appContext = appContext;
+            _target2 = new List<Order>();
+            Instance = this;
+        }
+
+        #region Implementation of IPlayerOrderService
+        public ReadOnlyCollection<Order> Target2
+        {
+            get { return _target2.AsReadOnly(); }
+        }
+
+        public void AddOrder(Order order)
+        {
+            if (order == null)
+                return;
+
+            var localPlayer = _appContext.LocalPlayer;
+            if (localPlayer == null)
+                return;
+
+            var owner = localPlayer.Empire;
+            if (owner == null)
+                return;
+
+            order.Owner = owner;
+
+            lock (_target2)
+            {
+                while ((_target2.Count > 0) && order.Overrides(_target2[_target2.Count - 1]))
+                    _target2.RemoveAt(_target2.Count - 1);
+                _target2.Add(order);
+            }
+            OnOrdersChanged();
+        }
+
+        public bool RemoveOrder(Order order)
+        {
+            if (order == null)
+                return false;
+            bool result;
+            lock (_target2)
+            {
+                result = _target2.Remove(order);
+            }
+            OnOrdersChanged();
+            return result;
+        }
+
+        public void ClearTarget2()
+        {
+            lock (_target2)
+            {
+                _target2.Clear();
+            }
+            OnOrdersChanged();
+        }
+        #endregion
+
+        public event EventHandler Target2Changed;
+
+        private void OnOrdersChanged()
+        {
+            if (Target2Changed != null)
+            {
+                Target2Changed.Invoke(this, new EventArgs());
+            }
+        }
+    }
+}
---
+++ SupremacyCore/Client/IPlayerTarget2Service.cs
@@ -0,0 +1,71 @@
+// IPlayerOrderService.cs
+//
+// Copyright (c) 2009 Mike Strobel
+//
+// This source code is subject to the terms of the Microsoft Reciprocal License (Ms-RL).
+// For details, see <http://www.opensource.org/licenses/ms-rl.html>.
+//
+// All other rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+using Supremacy.Annotations;
+using Supremacy.Game;
+
+namespace Supremacy.Client
+{
+    public interface IPlayerTarget2Service
+    {
+        ReadOnlyCollection<Order> Target2 { get; }
+        void AddOrder(Order target);
+        bool RemoveOrder(Order target);
+        void ClearTarget2();
+
+        bool AutoTurnTarget2 { get; set; }
+
+        event EventHandler Target2Changed;
+    }
+
+    public static class PlayerTarget2ServiceExtensions
+    {
+        public static int RemoveOrders([NotNull] this IPlayerTarget2Service orderService, [NotNull] IEnumerable<Order> target2)
+        {
+            if (orderService == null)
+                throw new ArgumentNullException("orderService");
+            if (target2 == null)
+                throw new ArgumentNullException("target2");
+
+            return target2.Count(orderService.RemoveOrder);
+        }
+
+        public static int RemoveOrders<TOrder>([NotNull] this IPlayerTarget2Service orderService, [NotNull] Func<TOrder, bool> predicate)
+            where TOrder : Order
+        {
+            if (orderService == null)
+                throw new ArgumentNullException("orderService");
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+
+            var removedCount = 0;
+            var target2 = orderService.Target2;
+
+            for (var i = 0; i < target2.Count; i++)
+            {
+                var target = target2[i] as TOrder;
+                if (target == null)
+                    continue;
+                if (!predicate(target))
+                    continue;
+                if (!orderService.RemoveOrder(target))
+                    continue;
+                --i;
+                ++removedCount;
+            }
+
+            return removedCount;
+        }
+    }
+}
