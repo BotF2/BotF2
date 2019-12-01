b0ae978a02ee383905ca04126c26b8bc26db163b
Reginald <bote-reginald@gmx.de>
2019-12-01 10:48:35 +0100

    SabotageEnergy needs more work, Sabotage Food and StealResearch should not causing a crash

--- SupremacyCore/Intelligence/IntelHelper.cs
+++ SupremacyCore/Intelligence/IntelHelper.cs
@@ -128,78 +128,81 @@ namespace Supremacy.Intelligence
 
         public static void SabotageFood(Colony colony, Civilization civ)
         {
-            var system = colony.System;
-            var attackedCiv = GameContext.Current.CivilizationManagers[colony.System.Owner];
-            var spyEmpire = IntelHelper.NewSpyCiv;
-            int ratio = -1;
-            if (spyEmpire == null)
-                return;
 
-            if (colony == null)
-                return;
+            GameLog.Core.Intel.DebugFormat("##### SabotageFood not implemented yet");
 
-            bool ownedByPlayer = (colony.OwnerID == spyEmpire.CivID);
-            if (ownedByPlayer)
-                return;
+            //var system = colony.System;
+            //var attackedCiv = GameContext.Current.CivilizationManagers[colony.System.Owner];
+            //var spyEmpire = IntelHelper.NewSpyCiv;
+            //int ratio = -1;
+            //if (spyEmpire == null)
+            //    return;
 
-            int defenseIntelligence = attackedCiv.TotalIntelligenceProduction + 1;  // TotalIntelligence of attacked civ
-            if (defenseIntelligence - 1 < 0.1)
-                defenseIntelligence = 2;
+            //if (colony == null)
+            //    return;
 
-            int attackingIntelligence = GameContext.Current.CivilizationManagers[spyEmpire].TotalIntelligenceProduction + 1;  // TotalIntelligence of attacked civ
-            if (attackingIntelligence - 1 < 0.1)
-                attackingIntelligence = 1;
+            //bool ownedByPlayer = (colony.OwnerID == spyEmpire.CivID);
+            //if (ownedByPlayer)
+            //    return;
 
-            attackingIntelligence = 100 * attackingIntelligence;// just for increase attacking Intelligence
+            //int defenseIntelligence = attackedCiv.TotalIntelligenceProduction + 1;  // TotalIntelligence of attacked civ
+            //if (defenseIntelligence - 1 < 0.1)
+            //    defenseIntelligence = 2;
 
-            ratio = attackingIntelligence / defenseIntelligence;
-            if (ratio < 2)
-                ratio = 1;   // we start with sabotage with ratio more than one, not before
+            //int attackingIntelligence = GameContext.Current.CivilizationManagers[spyEmpire].TotalIntelligenceProduction + 1;  // TotalIntelligence of attacked civ
+            //if (attackingIntelligence - 1 < 0.1)
+            //    attackingIntelligence = 1;
 
-            GameLog.Core.Intel.DebugFormat("owner= {0}, system= {1} is SABOTAGED by civ= {2} (Intelligence: defense={3}, attack={4}, ratio={5})",
-                system.Owner, system.Name, civ.Name, defenseIntelligence, attackingIntelligence, ratio);
+            //attackingIntelligence = 100 * attackingIntelligence;// just for increase attacking Intelligence
+
+            //ratio = attackingIntelligence / defenseIntelligence;
+            //if (ratio < 2)
+            //    ratio = 1;   // we start with sabotage with ratio more than one, not before
+
+            //GameLog.Core.Intel.DebugFormat("owner= {0}, system= {1} is SABOTAGED by civ= {2} (Intelligence: defense={3}, attack={4}, ratio={5})",
+            //    system.Owner, system.Name, civ.Name, defenseIntelligence, attackingIntelligence, ratio);
 
 
-            GameLog.Core.Intel.DebugFormat("Owner= {0}, system= {1} at {2} (sabotaged): Energy=? out of facilities={3}, in total={4}",
-                system.Owner, system.Name, system.Location,
-                //colony.GetEnergyUsage(),
-                system.Colony.GetActiveFacilities(ProductionCategory.Food),
-                system.Colony.GetTotalFacilities(ProductionCategory.Food));
-            GameLog.Core.Intel.DebugFormat("Sabotage FOOD to {0}: TotalFoodFacilities before={1}",
-                system.Name, colony.GetTotalFacilities(ProductionCategory.Food));
+            //GameLog.Core.Intel.DebugFormat("Owner= {0}, system= {1} at {2} (sabotaged): Energy=? out of facilities={3}, in total={4}",
+            //    system.Owner, system.Name, system.Location,
+            //    //colony.GetEnergyUsage(),
+            //    system.Colony.GetActiveFacilities(ProductionCategory.Food),
+            //    system.Colony.GetTotalFacilities(ProductionCategory.Food));
+            //GameLog.Core.Intel.DebugFormat("Sabotage FOOD to {0}: TotalFoodFacilities before={1}",
+            //    system.Name, colony.GetTotalFacilities(ProductionCategory.Food));
 
-            //Effect of sabatoge
-            int removeFoodFacilities = 0;
+            ////Effect of sabatoge
+            //int removeFoodFacilities = 0;
 
-            //if ratio > 1 than remove one more  EnergyFacility
-            if (ratio > 1 && colony.GetTotalFacilities(ProductionCategory.Food) > 1)// Food: remaining everything down to 1, for ratio: first value > 1 is 2, so ratio must be 2 or more
-            {
-                removeFoodFacilities = 1;
-                colony.RemoveFacilities(ProductionCategory.Food, 1);
-            }
+            ////if ratio > 1 than remove one more  EnergyFacility
+            //if (ratio > 1 && colony.GetTotalFacilities(ProductionCategory.Food) > 1)// Food: remaining everything down to 1, for ratio: first value > 1 is 2, so ratio must be 2 or more
+            //{
+            //    removeFoodFacilities = 1;
+            //    colony.RemoveFacilities(ProductionCategory.Food, 1);
+            //}
 
-            //if ratio > 2 than remove one more  FoodFacility
-            if (ratio > 2 && system.Colony.GetTotalFacilities(ProductionCategory.Food) > 2)// Food: remaining everything down to 1, for ratio: first value > 1 is 2, so ratio must be 2 or more
-            {
-                removeFoodFacilities = 3;  //  2 and one from before
-                system.Colony.RemoveFacilities(ProductionCategory.Food, 2);
-            }
+            ////if ratio > 2 than remove one more  FoodFacility
+            //if (ratio > 2 && system.Colony.GetTotalFacilities(ProductionCategory.Food) > 2)// Food: remaining everything down to 1, for ratio: first value > 1 is 2, so ratio must be 2 or more
+            //{
+            //    removeFoodFacilities = 3;  //  2 and one from before
+            //    system.Colony.RemoveFacilities(ProductionCategory.Food, 2);
+            //}
 
-            // if ratio > 3 than remove one more  FoodFacility
-            if (ratio > 3 && system.Colony.GetTotalFacilities(ProductionCategory.Food) > 3)// Food: remaining everything down to 1, for ratio: first value > 1 is 2, so ratio must be 2 or more
-            {
-                removeFoodFacilities = 6;  //   3 and 3 from before = 6 in total , max 6 should be enough for one sabotage ship
-                system.Colony.RemoveFacilities(ProductionCategory.Food, 3);
-            }
+            //// if ratio > 3 than remove one more  FoodFacility
+            //if (ratio > 3 && system.Colony.GetTotalFacilities(ProductionCategory.Food) > 3)// Food: remaining everything down to 1, for ratio: first value > 1 is 2, so ratio must be 2 or more
+            //{
+            //    removeFoodFacilities = 6;  //   3 and 3 from before = 6 in total , max 6 should be enough for one sabotage ship
+            //    system.Colony.RemoveFacilities(ProductionCategory.Food, 3);
+            //}
 
-            GameLog.Core.Intel.DebugFormat("Sabotage Food at {0}: TotalFoodFacilities after={1}", system.Name, colony.GetTotalFacilities(ProductionCategory.Food));
-            attackedCiv.SitRepEntries.Add(new NewSabotageSitRepEntry(civ, system.Colony, removeFoodFacilities, system.Colony.GetTotalFacilities(ProductionCategory.Food)));
+            //GameLog.Core.Intel.DebugFormat("Sabotage Food at {0}: TotalFoodFacilities after={1}", system.Name, colony.GetTotalFacilities(ProductionCategory.Food));
+            //attackedCiv.SitRepEntries.Add(new NewSabotageSitRepEntry(civ, system.Colony, removeFoodFacilities, system.Colony.GetTotalFacilities(ProductionCategory.Food)));
 
         }
 
         internal static void StealResearch(Colony colony, Civilization spiedFourCiv)
         {
-            throw new NotImplementedException();
+            GameLog.Core.Intel.DebugFormat("##### StealResearch not implemented yet");
         }
         #endregion
     }
