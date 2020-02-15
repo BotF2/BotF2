0f6b43c2f3f0aac6f72596446d83a8881c9eb572
Reginald <bote-reginald@gmx.de>
2020-02-15 15:32:08 +0100

    forget the SpyOps files

---
+++ SupremacyCore/SpyOperations/ISpyOrder.cs
@@ -0,0 +1,11 @@
+﻿namespace Supremacy.Diplomacy.Visitors
+{
+    public interface ISpyOrderAttacker
+    {
+        //void VisitGift(IProposal proposal);
+        void VisitDemand(ISpyOrder spyOrder);   // Steal Credits
+        //void VisitExchange(IProposal proposal);
+        //void VisitWarPact(IProposal proposal);
+        //void VisitTreatyProposal(IProposal proposal);
+    }
+}
---
+++ SupremacyCore/SpyOperations/Spy_2_SpyStatus.cs
@@ -0,0 +1,23 @@
+﻿namespace Supremacy.Diplomacy
+{
+    public enum Spy_2_SpyStatus : byte
+    {
+        NoSpyStatus = 0
+
+        // old stuff from Diplomacy
+
+        //OwnerIsSubjugated,
+        //CounterpartyIsSubjugated,
+        //AtWar,
+        //Neutral,
+        //Peace,
+        //Friendly,
+        //Affiliated,
+        //OwnerIsMember,
+        //CounterpartyIsMember,
+        //Allied,
+        //Self,
+        //OwnerIsUnreachable,
+        //CounterpartyIsUnreachable
+    }
+}
