94417f841e4df28555bef3f59036ba71c262acdb
Reginald <bote-reginald@gmx.de>
2020-04-18 13:45:39 +0200

    avoiding error messages during Build

--- SupremacyClientComponents/Context/Commands/GalaxyScreenCommands.cs
+++ SupremacyClientComponents/Context/Commands/GalaxyScreenCommands.cs
@@ -1,4 +1,4 @@
-// GalaxyScreenCommands.cs
+// File:GalaxyScreenCommands.cs
 //
 // Copyright (c) 2009 Mike Strobel
 //
@@ -133,7 +133,7 @@ namespace Supremacy.Client.Commands
         }
 
         #region Implementation of ICheckableCommandParameter
-        public event EventHandler InnerParameterChanged;
+        //public event EventHandler InnerParameterChanged;
         public event EventHandler IsCheckedChanged;
 
         public bool Handled { get; set; }
