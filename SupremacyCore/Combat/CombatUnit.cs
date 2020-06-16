546d22809c2e29650dc5e86844887d5b4d917cfe
Daedalus86 <email@justinvanbeusekom.nl>
2020-06-16 20:44:40 +0200

    Some code improvements according to Visual Studio (SupremacyCore -> Combat)

--- SupremacyCore/Combat/CombatUnit.cs
+++ SupremacyCore/Combat/CombatUnit.cs
@@ -21,63 +21,53 @@ namespace Supremacy.Combat
     public class CombatUnit : IEquatable<CombatUnit>
     {
         private readonly int _sourceId;
-        private readonly int _ownerId;
         private int _hullStrength;
         private int _shieldStrength;
-        private int _firepower;
-        private int _remainingFirepower;
         private bool _isCloaked;
         private bool _isCamouflaged;
-        private bool _isAssimilated;
-        private readonly string _name;
-        private int _cloakStrength = 0;
-        private int _camouflagedStrength = 0;
-        private int _scanStrength = 0;
-        private double _accuracy = 0d;
-        private double _damageControl = 0d;
 
-        protected CombatUnit(System.Collections.Generic.IEnumerable<Ship> ship) { }
+        //protected CombatUnit(System.Collections.Generic.IEnumerable<Ship> ship) { }
 
         public CombatUnit()
         {
-
         }
         public CombatUnit(Orbital source)
         {
             if (source == null)
-                throw new ArgumentNullException("source");
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
 
-            var ship = source as Ship;
+            Ship ship = source as Ship;
             if (ship != null)
             {
                 _isCloaked = ship.IsCloaked;
                 _isCamouflaged = ship.IsCamouflaged;
-                _isAssimilated = ship.IsAssimilated;
+                IsAssimilated = ship.IsAssimilated;
                 if (source.CloakStrength != null)
-                    _cloakStrength = source.CloakStrength.CurrentValue;
+                {
+                    CloakStrength = source.CloakStrength.CurrentValue;
+                }
+
                 if (source.CamouflagedMeter != null)
-                    _camouflagedStrength = source.CamouflagedMeter.CurrentValue;
+                {
+                    CamouflagedStrength = source.CamouflagedMeter.CurrentValue;
+                }
             }
             _sourceId = source.ObjectID;
-            _ownerId = source.OwnerID;
+            OwnerID = source.OwnerID;
             _hullStrength = source.HullStrength.CurrentValue;
-            _firepower = (source.OrbitalDesign.PrimaryWeapon.Damage * source.OrbitalDesign.PrimaryWeapon.Count) + (source.OrbitalDesign.SecondaryWeapon.Damage * source.OrbitalDesign.SecondaryWeapon.Count);
-            _remainingFirepower = _firepower;
+            Firepower = (source.OrbitalDesign.PrimaryWeapon.Damage * source.OrbitalDesign.PrimaryWeapon.Count) + (source.OrbitalDesign.SecondaryWeapon.Damage * source.OrbitalDesign.SecondaryWeapon.Count);
+            RemainingFirepower = Firepower;
             _shieldStrength = source.ShieldStrength.CurrentValue;
-            _name = source.Name;
-            _accuracy = source.GetAccuracyModifier();
-            _damageControl = source.GetDamageControlModifier();
+            Name = source.Name;
+            Accuracy = source.GetAccuracyModifier();
+            DamageControl = source.GetDamageControlModifier();
         }
 
-        public Orbital Source
-        {
-            get { return GameContext.Current.Universe.Get<Orbital>(_sourceId); }
-        }
+        public Orbital Source => GameContext.Current.Universe.Get<Orbital>(_sourceId);
 
-        public string Name
-        {
-            get { return _name; }
-        }
+        public string Name { get; }
 
         public string Description
         {
@@ -89,64 +79,32 @@ namespace Supremacy.Combat
                 {
                     return ResourceManager.GetString("COMBAT_DESCRIPTION_STATION");
                 }
-                if (Source is Ship)
+                if (Source is Ship ship)
                 {
-                    return String.Format(
+                    return string.Format(
                         ResourceManager.GetString("COMBAT_DESCRIPTION_SHIP"),
-                        ((Ship)Source).ShipDesign.ClassName,
-                        GameContext.Current.Tables.EnumTables["ShipType"][((Ship)Source).ShipType.ToString()][0]);
+                        ship.ShipDesign.ClassName,
+                        GameContext.Current.Tables.EnumTables["ShipType"][ship.ShipType.ToString()][0]);
                 }
                 return null;
             }
         }
 
-        public Civilization Owner
-        {
-            get { return GameContext.Current.Civilizations[_ownerId]; }
-        }
+        public Civilization Owner => GameContext.Current.Civilizations[OwnerID];
 
-        public int OwnerID
-        {
-            get { return _ownerId; }
-        }
+        public int OwnerID { get; }
 
-        public int Firepower
-        {
-            get
-            {
-                return _firepower;
-            }
-        }
+        public int Firepower { get; }
 
-        public int RemainingFirepower
-        {
-            get
-            {
-                return _remainingFirepower;
-            }
-            set
-            {
-                _remainingFirepower = value;
-            }
-        }
+        public int RemainingFirepower { get; set; }
 
-        public double Accuracy
-        {
-            get { return _accuracy; }
-        }
+        public double Accuracy { get; }
 
-        public double DamageControl
-        {
-            get { return _damageControl; }
-        } 
-        public int HullStrength
-        {
-            get { return _hullStrength; }
-        }
+        public double DamageControl { get; }
+        public int HullStrength => _hullStrength;
 
         public Percentage HullIntegrity
         {
-
             get
             {
                 if (HullStrength == 0)
@@ -154,14 +112,13 @@ namespace Supremacy.Combat
                     return 0;
                 }
                 else
-                return ((float)HullStrength / Source.OrbitalDesign.HullStrength);
+                {
+                    return (float)HullStrength / Source.OrbitalDesign.HullStrength;
+                }
             }
         }
 
-        public int ShieldStrength
-        {
-            get { return _shieldStrength; }
-        }
+        public int ShieldStrength => _shieldStrength;
 
         public Percentage ShieldIntegrity
         {
@@ -174,51 +131,26 @@ namespace Supremacy.Combat
                 else
                 {
                     GameLog.Client.Test.DebugFormat("ShieldStrenth crash owner ={0} {1} {2}", Source.Owner.Key, Source.Design.Key, Source.ShieldStrength.CurrentValue);
-                    { return ((float)ShieldStrength / Source.OrbitalDesign.ShieldStrength); }
+                    { return (float)ShieldStrength / Source.OrbitalDesign.ShieldStrength; }
                 }
             }
         }
 
-        public bool IsCloaked
-        {
-            get { return _isCloaked; }
-        }
+        public bool IsCloaked => _isCloaked;
 
-        public int CloakStrength
-        {
-            get { return _cloakStrength; }
-        }
+        public int CloakStrength { get; }
 
-        public bool IsCamouflaged
-        {
-            get { return _isCamouflaged; }
-        }
+        public bool IsCamouflaged => _isCamouflaged;
 
-        public int CamouflagedStrength
-        {
-            get { return _camouflagedStrength; }
-        }
+        public int CamouflagedStrength { get; }
 
-        public int ScanStrength
-        {
-            get { return _scanStrength; }
-        }
+        public int ScanStrength { get; }
 
-        public bool IsDestroyed
-        {
-            get { return (HullStrength <= 0); }
-        }
+        public bool IsDestroyed => HullStrength <= 0;
 
-        public bool IsAssimilated
-        {
-            get { return _isAssimilated; }
-            set {_isAssimilated = value; }
-        }
+        public bool IsAssimilated { get; set; }
 
-        public bool IsMobile
-        {
-            get { return Source.IsMobile; }
-        }
+        public bool IsMobile => Source.IsMobile;
 
         public void Cloak()
         {
@@ -233,7 +165,7 @@ namespace Supremacy.Combat
         {
             _isCloaked = false;
         }
-   
+
         public void Decamouflage()
         {
             _isCamouflaged = false;
@@ -241,7 +173,7 @@ namespace Supremacy.Combat
 
         public void TakeDamage(int damage)
         {
-            var remainingDamage = damage;
+            int remainingDamage = damage;
             if (_shieldStrength > 0)
             {
                 remainingDamage = Math.Max(0, remainingDamage - _shieldStrength);
@@ -252,15 +184,19 @@ namespace Supremacy.Combat
 
         public void UpdateSource()
         {
-            var source = Source;
+            Orbital source = Source;
             if (source == null)
+            {
                 return;
+            }
 
             source.ShieldStrength.Reset(ShieldStrength);
             source.HullStrength.Reset(HullStrength);
 
             if (!source.HullStrength.IsMinimized)
+            {
                 source.RegenerateShields();
+            }
         }
 
         public static bool operator ==(CombatUnit left, CombatUnit right)
@@ -273,13 +209,19 @@ namespace Supremacy.Combat
             return !Equals(left, right);
         }
 
-        public bool Equals(CombatUnit combatUnit)
+        public bool Equals(CombatUnit other)
         {
-            if (ReferenceEquals(combatUnit, this))
+            if (ReferenceEquals(other, this))
+            {
                 return true;
-            if (ReferenceEquals(combatUnit, null))
+            }
+
+            if (other is null)
+            {
                 return false;
-            return (_sourceId == combatUnit._sourceId);
+            }
+
+            return _sourceId == other._sourceId;
         }
 
         public override bool Equals(object obj)
