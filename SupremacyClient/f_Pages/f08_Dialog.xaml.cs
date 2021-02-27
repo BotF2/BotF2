e0ebe89ba98c2805220094ce40f9c0ba3b1e9828
Reginald <bote-reginald@gmx.de>
2021-02-27 15:22:12 +0100

    worked on F-Dialogs, new: F12

--- SupremacyClient/f_Pages/f08_Dialog.xaml.cs
+++ SupremacyClient/f_Pages/f08_Dialog.xaml.cs
@@ -7,18 +7,62 @@
 //
 // All other rights reserved.
 
+using Microsoft.Practices.ServiceLocation;
+using Supremacy.Client.Context;
+using Supremacy.Game;
+using Supremacy.Orbitals;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Supremacy.Client
 {
+
     /// <summary>
     /// Interaction logic for f08_Dialog.xaml.cs
     /// </summary>
-    public partial class F08_Dialog
+    public partial class F08_Dialog 
     {
+
+        //private string _text = "text";
+        //private string F08_Text_1 = "IsHitTestVisibleProperty";
+        private List<Fleet> _allFleets;
+        //private Fleet _testFleet;
+        private string T2;
+        private IAppContext _appContext;
+
+        //GameContext.Current.
+
+        //_allFleets = GameContext.Current.Universe.Find<Fleet>().ToList();
         public F08_Dialog()
         {
             InitializeComponent();
+
+            _appContext = ServiceLocator.Current.GetInstance<IAppContext>();
+            DataTemplate itemTemplate = TryFindResource("AssetsTreeItemTemplate") as DataTemplate;
+
+            //string t1 = F08_Text_1;
+            string t2 = F08_Text_2;
+            T2 = t2;
+
+            if (GameContext.Current != null)
+                _allFleets = GameContext.Current.Universe.Find<Fleet>().ToList();
+
+            //_testFleet = new Fleet();
+            //_testFleet.TurnCreated = 1;
+            //DataContext = _testFleet;
+
+            if (_allFleets != null)
+                DataContext = _allFleets;
+
+            //T1.Value = "hel";
+            //string t3 = F08_Text_3;
+
+            //string F08_Text_1 = "IsHitTestVisibleProperty";
+            //string F08_Text_3 = "IsHitTestVisibleProperty";
+            //string Empire = "IsHitTestVisibleProperty";
+            //_allFleets = GameContext.Current.Universe.Find<Fleet>().ToList();
 
             InputBindings.Add(
                 new KeyBinding(
@@ -42,20 +86,51 @@ namespace Supremacy.Client
                     GenericCommands.AcceptCommand,
                     OnGenericCommandsAcceptCommandExecuted));
 
-            CommandBindings.Add(
-                new CommandBinding(
-                    GenericCommands.TracesSetAllwithoutDetailsCommand,
-                    OnGenericCommandsTracesSetAllwithoutDetailsCommandExecuted));
+            //CommandBindings.Add(
+            //    new CommandBinding(
+            //        GenericCommands.TracesSetAllwithoutDetailsCommand,
+            //        OnGenericCommandsTracesSetAllwithoutDetailsCommandExecuted));
 
-            CommandBindings.Add(
-                new CommandBinding(
-                    GenericCommands.TracesSetSomeCommand,
-                    OnGenericCommandsTracesSetSomeCommandExecuted));
+            //CommandBindings.Add(
+            //    new CommandBinding(
+            //        GenericCommands.TracesSetSomeCommand,
+            //        OnGenericCommandsTracesSetSomeCommandExecuted));
 
-            CommandBindings.Add(
-                new CommandBinding(
-                    GenericCommands.TracesSetNoneCommand,
-                    OnGenericCommandsTracesSetNoneCommandExecuted));
+            //CommandBindings.Add(
+            //    new CommandBinding(
+            //        GenericCommands.TracesSetNoneCommand,
+            //        OnGenericCommandsTracesSetNoneCommandExecuted));
+
+
+        }
+
+        private class F08_Dialog_Class
+        {
+            //string _f08_Text_1 = "IsHitTestVisibleProperty";
+            //public string F08_Text_1
+            //{
+            //    get { return "hello 8" /*_text1*/; }
+            //}
+            //    private class F08_Text_1 { return "hello 8" /*_text1*/; }
+
+            //}
+
+
+        }
+
+        public string F08_Text_1
+        {
+            get { return "hello 8" /*_text1*/; }
+        }
+
+        public string F08_Text_2
+        {
+            get { return "hello 8" /*_text1*/; }
+        }
+
+        private string F08_Text_3
+        {
+            get { return "hello 8" /*_text1*/; }
         }
 
         private void OnGenericCommandsCancelCommandExecuted(object source, ExecutedRoutedEventArgs e)
@@ -75,29 +150,29 @@ namespace Supremacy.Client
             Close();
         }
 
-        private void OnGenericCommandsTracesSetAllwithoutDetailsCommandExecuted(object source, ExecutedRoutedEventArgs e)
-        {
-            ClientSettings.Current.TracesAudio = true;
+        //private void OnGenericCommandsTracesSetAllwithoutDetailsCommandExecuted(object source, ExecutedRoutedEventArgs e)
+        //{
+        //    ClientSettings.Current.TracesAudio = true;
 
-            ClientSettings.Current.Save();
-            ClientSettings.Current.Reload();
-        }
+        //    ClientSettings.Current.Save();
+        //    ClientSettings.Current.Reload();
+        //}
 
-        private void OnGenericCommandsTracesSetSomeCommandExecuted(object source, ExecutedRoutedEventArgs e)
-        {
-            ClientSettings.Current.TracesAudio = false;
+        //private void OnGenericCommandsTracesSetSomeCommandExecuted(object source, ExecutedRoutedEventArgs e)
+        //{
+        //    ClientSettings.Current.TracesAudio = false;
 
-            ClientSettings.Current.Save();
-            ClientSettings.Current.Reload();
-        }
+        //    ClientSettings.Current.Save();
+        //    ClientSettings.Current.Reload();
+        //}
 
-        private void OnGenericCommandsTracesSetNoneCommandExecuted(object source, ExecutedRoutedEventArgs e)
-        {
-            //ClientSettings.Traces_ClearAllProperty();
-            ClientSettings.Current.TracesAudio = false;
+        //private void OnGenericCommandsTracesSetNoneCommandExecuted(object source, ExecutedRoutedEventArgs e)
+        //{
+        //    //ClientSettings.Traces_ClearAllProperty();
+        //    ClientSettings.Current.TracesAudio = false;
 
-            ClientSettings.Current.Save();
-            ClientSettings.Current.Reload();
-        }
+        //    ClientSettings.Current.Save();
+        //    ClientSettings.Current.Reload();
+        //}
     }
 }
